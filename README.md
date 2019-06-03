9f8e35660d7b3d1dec7007314ad6f959547d9956
AmritSidhu <amrit.sidhu@digital.cabinet-office.gov.uk>
2019-06-03 11:05:15 +0100

    complete-synoptic-project

---
+++ README.md
@@ -0,0 +1,27 @@
+## Setting up the application
+
+To be able to run the app simply run the following commands below:
+```
+bundle install
+```
+This will install all the gems(dependenices) requried by the application
+```
+rails db:migrate
+```
+This will setup the database and table for you due to the migration scripts already in place
+
+## Runnning the application
+
+To run the application run the command below:
+```
+rails s
+```
+Then visit `localhost:3000` in your browser
+
+## Running tests
+
+To run the rests run the command below:
+```
+rails t
+```
+
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,38 @@
+class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in @user
+      flash[:success] = "You have now registered"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+    else
+      render 'show'
+    end
+  end
+
+  private
+
+    def user_params
+      params.require(:user).permit(:name, :email, :mobile, :employeeid, :password, :password_confirmation, :balance)
+    end
+end
